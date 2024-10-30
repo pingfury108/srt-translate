a732c92ae13c850f388a987c163c648abdc93940
pingfury <pingfury@outlook.com>
2024-10-30 15:37:38 +0800

    to cli

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,95 @@
-#![allow(non_snake_case)]
+use clap::Parser;
+use dify_client::{request, Client, Config};
+use indicatif::ProgressBar;
+use srtlib::{Subtitle, Subtitles};
+use std::{collections::HashMap, time::Duration};
 
-use dioxus::prelude::*;
-use dioxus_logger::tracing::{info, Level};
+/// srt-translate subtitle translation tool
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(long, default_value_t = String::from("zh"),)]
+    target_lang: String,
 
-//const TAILWIND_URL: Asset = asset!("assets/tailwind.css");
+    #[arg(short, long)]
+    src_file: String,
 
-fn main() {
-    // Init logger
-    dioxus_logger::init(Level::INFO).expect("failed to init logger");
-    info!("starting app");
-    launch(App);
+    #[arg(short, long)]
+    to_file: String,
+
+    #[arg(short, long)]
+    dify_app_token: String,
+
+    #[arg(long, default_value_t = String::from("https://api.dify.ai"))]
+    dify_base_url: String,
 }
 
-#[component]
-fn App() -> Element {
-    // Build cool things ✌️
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    let args = Args::parse();
+    let subs = Subtitles::parse_from_file(args.src_file, None).unwrap();
 
-    rsx! {
-        div {
-            class: "container mx-auto px-4",
-            div {
-                class: "grid grid-cols-2",
-                div {
-                    h1 {
-                        "字幕翻译"
-                    },
-                },
-                div {
-                    input {
-                        r#type: "file",
-                    }
-                }
+    let config = Config {
+        base_url: args.dify_base_url,
+        api_key: args.dify_app_token,
+        timeout: Duration::from_secs(60),
+    };
+    let client = Client::new_with_config(config);
+
+    let mut new_subs = Subtitles::new();
+    let subs_vec = subs.to_vec();
+    let subs_vec = &subs_vec[0..50];
+
+    let bar = ProgressBar::new(subs_vec.len() as u64);
+    for (index, s) in subs_vec.iter().enumerate() {
+        //log::debug!("{}", s.text)
+        bar.inc(1);
+        new_subs.push(s.clone());
+
+        let above_text: String = {
+            if index == 0 {
+                "".to_string()
+            } else {
+                subs_vec[index - 1].text.clone()
             }
-            div {
+        };
 
-                table {
-                    class: "table-auto",
-                    thead {
-                        tr {
-                            th {"index"},
-                            th {"开始时间"},
-                            th {"结束时间"},
-                            th {"原文"},
-                            th {"译文(可编辑)"},
-                        }
-                    },
-                    tbody {
-                        tr{
-                            th{"0"},
-                            td {"00:00:04"},
-                            td {"00:00:04"},
-                            th{"MUSIC"},
-                            th{input {"音乐"}},
-                        },
-                    }
-                }
+        let below_text: String = {
+            if index == subs_vec.len() - 1 {
+                "".to_string()
+            } else {
+                subs_vec[index + 1].text.clone()
+            }
+        };
 
+        let data = request::CompletionMessagesRequest {
+            user: "srt-translate".into(),
+            response_mode: request::ResponseMode::Blocking,
+            inputs: {
+                let mut input = HashMap::new();
+                input.insert("query".into(), s.text.clone());
+                input.insert("lang".into(), args.target_lang.clone());
+                input.insert("above".into(), above_text);
+                input.insert("below".into(), below_text);
+
+                input
+            },
+            ..Default::default()
+        };
+
+        let result = client.api().completion_messages(data).await;
+        match result {
+            Ok(r) => new_subs.push(Subtitle::new(s.num, s.start_time, s.end_time, r.answer)),
+            Err(e) => {
+                log::error!("{}", e)
             }
         }
     }
+    new_subs.sort();
+    bar.finish();
+    //println!("{}", new_subs);
+    new_subs
+        .write_to_file("ttt.srt", None)
+        .expect("write new srt file");
 }
